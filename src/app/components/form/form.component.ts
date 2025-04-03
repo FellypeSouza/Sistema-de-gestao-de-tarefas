505f9c04a8655e84fe59c7c680857d79f48b4799
FellypeSouza <fellypedev23@gmail.com>
2025-04-03 18:03:51 -0300

    commit

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  inputNome: string = "";
+  inputSenha:string = "";
 
+  Login(inputNome:string, inputSenha:string):void{
+    if (inputNome == "" || inputSenha=="") {
+        alert("Preencha todos os campos");
+    }
+    else{
+      alert(inputNome);
+      alert(inputSenha);
+    }
+  }
 }
